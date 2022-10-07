55a7dbbb43d6b5a64f551970ee127ea574583133
joymush <joy.chemutai@student.moringaschool.com>
2022-10-07 12:54:41 +0300

    create a event handler

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,5 +5,13 @@ const Item = ({ items, completeItem, removeItem, updateItem }) => {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
-    })
+    });
+
+    const submitUpdate = value => {
+        updateItem(edit.id, value);
+        setEdit({
+            id: null,
+            value: ''
+        });
+    }
 }
