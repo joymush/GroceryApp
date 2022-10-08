35d65922b8fb7005de3d7a60e55b7000fd77de89
joymush <joy.chemutai@student.moringaschool.com>
2022-10-08 23:08:09 +0300

    add links to home about itemlist

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import React from 'react';
 import './App.css';
 import ItemList from './components/ItemList';
 import {
-  BrowserRouter as Router
-}
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+function App () {
   return (
     <div className='grocery-app'>
       <ItemList />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/">ItemList</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/about">
+              
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
-}
 
+}
 export default App;
